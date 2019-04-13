0cdd06242487447838c3af020a872a0808f8d244
Julian <Julian@MAIN-PC>
2019-04-13 12:58:24 -0700

    stat

--- RLDashboard/settings.py
+++ RLDashboard/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/main/static/'
 
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['main/static']))
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'main/static'),
 )
